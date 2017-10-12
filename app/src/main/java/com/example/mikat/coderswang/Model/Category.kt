f35eb243444b455b02adf86a0d84033b7cf91390
mtleinon <mika.t.leinonen@gmail.com>
2017-10-12 19:41:24 +0300

    Added adapter.

--- app/src/main/java/com/example/mikat/coderswang/Model/Category.kt
+++ app/src/main/java/com/example/mikat/coderswang/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.mikat.coderswang.Model
 /**
  * Created by mikat on 12.10.2017.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
